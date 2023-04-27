bf1f5998853d176aa73b9120482f1711a82d929e
Anthony <yoshidesigns@outlook.com>
2023-04-27 15:49:18 -0400

    Switching to Linux dev env

--- app.go
+++ app.go
@@ -3,8 +3,11 @@ package main
 import (
 	"www.github.com/goid/modules/Code"
     "database/sql"
+    "crypto/tls"
+	"net/http"
     "github.com/gin-gonic/gin"
-	"fmt"	
+	"fmt"
+    "io"
 	_ "github.com/go-sql-driver/mysql"
 	// "os"
 )
@@ -20,7 +23,7 @@ func main() {
 	// 	DBName: os.Getenv("DBNAME"),
     // }
 
-    // Connect to the database
+    // Connect to the database - Currently uses a remote DB host (Local VM - Homestead)
     db, err := sql.Open("mysql", "homestead:secret@tcp(192.168.10.10:3306)/newtree")
     if err != nil {
         panic(err)
@@ -32,13 +35,70 @@ func main() {
     // fmt.Println("Connected!")
     defer db.Close()
 
+    // Create a new http.Transport with TLS settings
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{
+			InsecureSkipVerify: false, // InsecureSkipVerify should be set to false in production
+		},
+	}
+
+	// Create a new http.Client using the transport
+	client := &http.Client{
+		Transport: tr,
+	}
+
     // Initialize the Gin router
     router := gin.Default()
 
+	// Define a handler function for a GET request to the root URL
 	router.GET("/", func(c *gin.Context) {
-		var user goid.User
-		fmt.Println("Hello, Gin!")
+		// Send an HTTPS GET request to the server
+		resp, err := client.Get("http://myworldworks.com")
+		if err != nil {
+            fmt.Println("Oh noes!")
+            fmt.Println(err)
+			c.AbortWithError(http.StatusInternalServerError, err)
+			return
+		}
+        fmt.Println("Client Requested: ", c.Response.URL)
+        fmt.Println("Host: ", c.Response.Host)
 
+        // Get the TLS connection state
+        // state, ok := r.TLS
+        // if !ok {
+        //     http.Error(w, "No TLS connection", http.StatusBadRequest)
+        //     return
+        // }
+
+        fmt.Println("Get Anthony Lyristis.com")
+
+        body, err := io.ReadAll(resp.Body)
+        if err != nil {
+            // log.Fatal(err)
+        }
+
+        fmt.Printf("Code: %d\n", resp.StatusCode)
+        fmt.Printf("Body: %s\n", body)
+
+		// Copy the response body to the Gin context
+		defer resp.Body.Close()
+
+		c.Writer.WriteHeader(resp.StatusCode)
+		_, err = io.Copy(c.Writer, resp.Body)
+		if err != nil {
+            fmt.Println("Oh noes again!")
+            fmt.Println(err)
+			c.AbortWithError(http.StatusInternalServerError, err)
+			return
+		}
+        fmt.Println(resp.Body)
+        fmt.Println(body)
+        //return c.IndentedJSON(http.StatusOK, body)
+	})
+
+	router.GET("/check-user", func(c *gin.Context) {
+		var user goid.User
+	
 		err := db.QueryRow("SELECT id, name, email, password, token FROM users WHERE email = ?", "anthony@mail.com").Scan(&user.ID, &user.Name, &user.Email, &user.Password, &user.Token)
 		if err != nil {
 			fmt.Println(err)
